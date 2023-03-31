16b017644ebe3f5e81afd7138022569a25e1b1d3
Harry <lv000013@morris.umn.edu>
2023-03-31 12:24:22 -0500

    Set priority manually by volunteers

--- client/src/app/requests/request.ts
+++ client/src/app/requests/request.ts
@@ -1,10 +1,22 @@
 export interface Request {
+  id: any;
+priority: any;
+  requestPriority: number;
   _id: string;
   name: string; // Name of the form requester.
   itemType: ItemType;
   description: string;
   foodType: FoodType;
 }
+
+const priorityMap = {
+  vegetable: 1,
+  grain: 2,
+  fruit: 3,
+  meat: 4,
+  dairy: 5,
+};
+
 
 export type ItemType = 'food' | 'toiletries' | 'other';
 export type FoodType = '' | 'dairy' | 'grain' | 'meat' | 'fruit' | 'vegetable';
