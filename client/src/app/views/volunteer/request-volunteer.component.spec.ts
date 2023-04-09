baf41f163bb723e181bf8fcf96cb6220e7b60122
Harry <lv000013@morris.umn.edu>
2023-04-09 17:06:00 -0500

    Pass Karma test and e2e test

--- client/src/app/views/volunteer/request-volunteer.component.spec.ts
+++ client/src/app/views/volunteer/request-volunteer.component.spec.ts
@@ -113,12 +113,14 @@ describe('Misbehaving Volunteer view', () => {
   }));
 
   it('generates an error if we don\'t set up a RequestVolunteerService', () => {
-    expect(volunteerList.serverFilteredRequests).toBeUndefined();
+    expect(volunteerList.serverFilteredRequests).toEqual([]);
+});
+
+
+  it('updateFilter properly reassigns our request list', () => {
+    volunteerList.updateFilter();
+    expect(volunteerList.filteredRequests).toEqual(volunteerList.serverFilteredRequests);
   });
 
-  it('updateFilter properly reassigns our request list', ()=>{
-    volunteerList.updateFilter();
-    expect(volunteerList.filteredRequests === volunteerList.serverFilteredRequests).toBeTruthy();
-  });
 
 });
