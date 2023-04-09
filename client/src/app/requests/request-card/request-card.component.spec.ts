d31204ba60779b503bc0af8935244d744393bfaa
Harry <lv000013@morris.umn.edu>
2023-04-09 15:34:35 -0500

    Fix "Not unreachable" issues

--- client/src/app/requests/request-card/request-card.component.spec.ts
+++ client/src/app/requests/request-card/request-card.component.spec.ts
@@ -1,12 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Request } from '../request';
 import { RequestCardComponent } from './request-card.component';
-
 import { HttpClientModule } from '@angular/common/http';
 
 describe('RequestCardComponent', () => {
   let component: RequestCardComponent;
   let fixture: ComponentFixture<RequestCardComponent>;
+
+  const mockRequest: Request = {
+    _id: '100',
+    name: 'bob',
+    itemType: 'food',
+    description: 'I would like to be able to get some spaghetti noodles',
+    foodType: 'grain',
+    priority: 4
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,6 +25,7 @@ describe('RequestCardComponent', () => {
 
     fixture = TestBed.createComponent(RequestCardComponent);
     component = fixture.componentInstance;
+    component.request = mockRequest; // Set the request property
     fixture.detectChanges();
   });
 
